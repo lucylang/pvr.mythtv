d6739a165287c511285b5d4c3ee5f2744408a919
janbar <jlbarriere68@gmail.com>
2015-08-31 14:17:47 +0200

    create definitions for time values in right place
    tools.h

--- src/cppmyth/MythScheduleHelper75.cpp
+++ src/cppmyth/MythScheduleHelper75.cpp
@@ -524,7 +524,7 @@ bool MythScheduleHelper75::FillTimerEntryWithRule(MythTimerEntry& entry, const M
           // fill timeslot starting at next recording
           entry.startTime = rule.NextRecording(); // it includes offset correction
           // WARNING: if next recording has been overriden then offset could be different
-          timeadd(&entry.startTime, 60 * rule.StartOffset()); // remove start offset
+          timeadd(&entry.startTime, INTERVAL_MINUTE * rule.StartOffset()); // remove start offset
           entry.endTime = 0; // any time
         }
         else if (difftime(rule.LastRecorded(), 0) > 0)
@@ -532,7 +532,7 @@ bool MythScheduleHelper75::FillTimerEntryWithRule(MythTimerEntry& entry, const M
           // fill timeslot starting at last recorded
           entry.startTime = rule.LastRecorded(); // it includes offset correction
           // WARNING: if last recorded has been overriden then offset could be different
-          timeadd(&entry.startTime, 60 * rule.StartOffset()); // remove start offset
+          timeadd(&entry.startTime, INTERVAL_MINUTE * rule.StartOffset()); // remove start offset
           entry.endTime = 0; // any time
         }
       }
